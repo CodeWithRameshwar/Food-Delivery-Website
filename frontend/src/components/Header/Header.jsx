7c1a5a2a74de0eb581516de4c93a802e7eb1a546
Code_With_Rameshwar <rameshwar.vidhate001@gmail.com>
2026-02-17 20:24:19 +0530

    Initial commit: Setup backend and frontend structure

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+          <div className="header-contents">
+            <h2>
+                Order your Favourite food here
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, similique hic earum tenetur quisquam iusto error, consequatur molestias sapiente maxime beatae necessitatibus tempora harum ut atque ipsa. Vitae, libero ipsa.
+            </p>
+            <button>View Menu</button>
+          </div>
+        </div>
+    )
+}
+
+export default Header;
